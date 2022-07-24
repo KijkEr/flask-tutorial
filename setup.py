5552dd5c85cd3f997971f3fb17cf3dcabc719299
KijkEr <joep_is_cool@hotmail.com>
2022-07-24 16:40:26 +0200

    Tests en setup toegevoegd

---
+++ setup.py
@@ -0,0 +1,13 @@
+from jinja2 import TemplateRuntimeError
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_required=[
+        'flask',
+    ]
+)
